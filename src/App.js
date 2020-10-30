8f6d52c9df5c05a74dc0deb97d810ae1feaa9aa0
roberth67 <roberto_ara67@hotmail.com>
2020-10-30 16:41:08 -0700

    primer

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+import styled from '@emotion/styled';
+import Formulario from './components/Formulario';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
+
+function App() {
+  return (
+    <Fragment>
+      <Contenedor>
+      <Header 
+        titulo='Cotizador de Seguros'
+      />
+
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+      </Contenedor>
+    </Fragment>
+  );
+}
+
+export default App;
